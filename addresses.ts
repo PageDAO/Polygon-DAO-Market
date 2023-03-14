3e21726b8e82965b02d5d0e00452ac9359ae3a3d
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2023-03-13 22:40:13 -0500

    Update addresses.ts
    polygon contract

--- addresses.ts
+++ addresses.ts
@@ -1,2 +1,2 @@
 // Use your marketplace contract address here
-export const marketplaceContractAddress = '0x96fAecD31C70234402d5124aD2503F58822AF15d';
+export const marketplaceContractAddress = '0x73B44098e67f3B08A4080Ede4F3F6dA89316b92a';
